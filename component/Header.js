ee073bae40d6b99d4726399d6a419eb1288f4ef7
Bhavankumarg <gbhavanbkr@gmail.com>
2024-07-26 11:26:27 +0530

    headre

--- component/Header.js
+++ component/Header.js
@@ -1,132 +1,279 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+// import React from 'react'
+// import Link from 'next/link'
+// import Image from 'next/image'
+
+// function Header() {
+//   return (
+//     <div>
+//       <nav className="navbar navbar-expand-lg bg-body-tertiary header">
+//         <div className="container-fluid">
+//         <Link href="/" className="navbar-brand">
+//             <Image src='/images/cac_Logo.svg' width={200} height={50} alt='cac log' className='img_logo' />
+//           </Link>
+//     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//       <ul className="navbar-nav">
+//         <li className="nav-item">
+//                 <Link href="/" className="me-4 nav-link fs-6">Home</Link>
+//         </li>
+//         <li className="nav-item">
+//         <Link href="/about-us" className="me-4 nav-link fs-6">About Us</Link>
+//         </li>
+//         <li className="nav-item">
+//         <Link href="/action" className="me-4 nav-link fs-6">Our Action</Link>
+//               </li>
+//               <li className="nav-item">
+//               <Link href="/impact" className="me-4 nav-link fs-6">Our Impact</Link>
+//         </li>
+//         <li className="nav-item dropdown mx-1">
+//           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//           Our Initiatives
+//           </a>
+//                 <ul className="dropdown-menu">
+//                   <li><Link className='dropdown-item fs-6' href="/shilp">SHILP</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//                   <li><Link className='dropdown-item fs-6' href="/economic-resilience">Economic resilience</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//                   <li><Link className='dropdown-item fs-6' href="https://covidactioncollab.org/vaxnow/" target='_blank'>VaxNow</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//                   <li><Link className='dropdown-item fs-6' href="https://www.precisionhealth.in/" target='_blank'>Precision Health</Link></li>
+//           </ul>
+//               </li>
+//               <li className="nav-item dropdown mx-1">
+//           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//           Resource Hub
+//           </a>
+//                 <ul className="dropdown-menu">
+//                   <li><Link className='dropdown-item fs-6' href="/resource-hub/#base">Knowledgebase</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//             <li><Link className='dropdown-item fs-6' href="/resource-hub/#media">Media</Link></li>
+//           </ul>
+//               </li>
+//               <li className="nav-item">
+//               <Link className='me-4 nav-link fs-6' href="/join-us">Join Us</Link>
+//               </li>
+//               <li className="nav-item">
+//               <Link className='me-4 nav-link fs-6' href="/contact-us">Contact Us</Link>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+//       </nav>
+      
+//       <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+//   <div class="offcanvas-header">
+//   <Image src='./images/cac_Logo.svg' width={200} height={50} alt='cac log' className='img_logo'></Image>
+//     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+//   </div>
+//   <div class="offcanvas-body">
+//   <nav className="">
+//   <div className="container-fluid">
+//     <div className="navbar" >
+//                 <ul className="navbar-nav" style={{width:'100%'}}>
+//         <li className="nav-item"><Link href="/" className="me-4 nav-link fs-6">Home</Link></li>
+//               <hr/>
+//         <li className="nav-item">
+//         <Link href="/about-us" className="me-4 nav-link fs-6">About Us</Link>
+//                   </li>
+//         <hr/>
+//         <li className="nav-item">
+//         <Link href="/action" className="me-4 nav-link fs-6">Our Action</Link>
+//                   </li>
+//                   <hr/>         
+//               <li className="nav-item">
+//               <Link href="/impact" className="me-4 nav-link fs-6">Our Impact</Link>
+//                   </li>
+//                   <hr/>
+//         <li className="nav-item dropdown mx-1">
+//           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//           Our Initiatives
+//           </a>
+//                 <ul className="dropdown-menu">
+//                   <li><Link className='dropdown-item fs-6' href="/shilp">SHILP</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//                   <li><Link className='dropdown-item fs-6' href="/economic-resilience">Economic resilience</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//                   <li><Link className='dropdown-item fs-6' href="https://covidactioncollab.org/vaxnow/" target='_blank'>VaxNow</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//                   <li><Link className='dropdown-item fs-6' href="https://www.precisionhealth.in/" target='_blank'>Precision Health</Link></li>
+//           </ul>
+//               </li><hr/>
+//               <li className="nav-item dropdown mx-1">
+//           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//           Resource Hub
+//           </a>
+//                 <ul className="dropdown-menu">
+//                   <li><Link className='dropdown-item fs-6' href="/resource-hub/#base">Knowledgebase</Link></li>
+//                   <li><hr class="dropdown-divider"/></li>
+//             <li><Link className='dropdown-item fs-6' href="/resource-hub/#media">Media</Link></li>
+//           </ul>
+//               </li><hr/>
+//               <li className="nav-item">
+//               <Link className='me-4 nav-link fs-6' href="/join-us">Join Us</Link>
+//               </li><hr/>
+//               <li className="nav-item">
+//               <Link className='me-4 nav-link fs-6' href="/contact-us">Contact Us</Link>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+//       </nav>
+//   </div>
+// </div>
+
+
+
+//     </div>
+//   )
+// }
+
+// export default Header
+
+
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Header() {
+
+  const [offcanvasOpen, setOffcanvasOpen] = useState(false);
+
+  const toggleOffcanvas = () => {
+    setOffcanvasOpen(!offcanvasOpen);
+  };
+
+ 
+  const closeOffcanvas = () => {
+    setOffcanvasOpen(false);
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary header">
         <div className="container-fluid">
-        <Link href="/" className="navbar-brand">
+          <Link href="/" className="navbar-brand">
             <Image src='/images/cac_Logo.svg' width={200} height={50} alt='cac log' className='img_logo' />
           </Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" onClick={toggleOffcanvas}>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
                 <Link href="/" className="me-4 nav-link fs-6">Home</Link>
-        </li>
-        <li className="nav-item">
-        <Link href="/about-us" className="me-4 nav-link fs-6">About Us</Link>
-        </li>
-        <li className="nav-item">
-        <Link href="/action" className="me-4 nav-link fs-6">Our Action</Link>
               </li>
               <li className="nav-item">
-              <Link href="/impact" className="me-4 nav-link fs-6">Our Impact</Link>
-        </li>
-        <li className="nav-item dropdown mx-1">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Our Initiatives
-          </a>
-                <ul className="dropdown-menu">
-                  <li><Link className='dropdown-item fs-6' href="/shilp">SHILP</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
-                  <li><Link className='dropdown-item fs-6' href="/economic-resilience">Economic resilience</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
-                  <li><Link className='dropdown-item fs-6' href="https://covidactioncollab.org/vaxnow/" target='_blank'>VaxNow</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
-                  <li><Link className='dropdown-item fs-6' href="https://www.precisionhealth.in/" target='_blank'>Precision Health</Link></li>
-          </ul>
+                <Link href="/about-us" className="me-4 nav-link fs-6">About Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/action" className="me-4 nav-link fs-6">Our Action</Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/impact" className="me-4 nav-link fs-6">Our Impact</Link>
               </li>
               <li className="nav-item dropdown mx-1">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Resource Hub
-          </a>
-                <ul className="dropdown-menu">
-                  <li><Link className='dropdown-item fs-6' href="/resource-hub/#base">Knowledgebase</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
-            <li><Link className='dropdown-item fs-6' href="/resource-hub/#media">Media</Link></li>
-          </ul>
-              </li>
-              <li className="nav-item">
-              <Link className='me-4 nav-link fs-6' href="/join-us">Join Us</Link>
-              </li>
-              <li className="nav-item">
-              <Link className='me-4 nav-link fs-6' href="/contact-us">Contact Us</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-      </nav>
-      
-      <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-  <div class="offcanvas-header">
-  <Image src='./images/cac_Logo.svg' width={200} height={50} alt='cac log' className='img_logo'></Image>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-  <nav className="">
-  <div className="container-fluid">
-    <div className="navbar" >
-                <ul className="navbar-nav" style={{width:'100%'}}>
-        <li className="nav-item"><Link href="/" className="me-4 nav-link fs-6">Home</Link></li>
-              <hr/>
-        <li className="nav-item">
-        <Link href="/about-us" className="me-4 nav-link fs-6">About Us</Link>
-                  </li>
-        <hr/>
-        <li className="nav-item">
-        <Link href="/action" className="me-4 nav-link fs-6">Our Action</Link>
-                  </li>
-                  <hr/>         
-              <li className="nav-item">
-              <Link href="/impact" className="me-4 nav-link fs-6">Our Impact</Link>
-                  </li>
-                  <hr/>
-        <li className="nav-item dropdown mx-1">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Our Initiatives
-          </a>
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Our Initiatives
+                </a>
                 <ul className="dropdown-menu">
                   <li><Link className='dropdown-item fs-6' href="/shilp">SHILP</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
+                  <li><hr className="dropdown-divider" /></li>
                   <li><Link className='dropdown-item fs-6' href="/economic-resilience">Economic resilience</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
+                  <li><hr className="dropdown-divider" /></li>
                   <li><Link className='dropdown-item fs-6' href="https://covidactioncollab.org/vaxnow/" target='_blank'>VaxNow</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
+                  <li><hr className="dropdown-divider" /></li>
                   <li><Link className='dropdown-item fs-6' href="https://www.precisionhealth.in/" target='_blank'>Precision Health</Link></li>
-          </ul>
-              </li><hr/>
+                </ul>
+              </li>
               <li className="nav-item dropdown mx-1">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Resource Hub
-          </a>
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Resource Hub
+                </a>
                 <ul className="dropdown-menu">
                   <li><Link className='dropdown-item fs-6' href="/resource-hub/#base">Knowledgebase</Link></li>
-                  <li><hr class="dropdown-divider"/></li>
-            <li><Link className='dropdown-item fs-6' href="/resource-hub/#media">Media</Link></li>
-          </ul>
-              </li><hr/>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link className='dropdown-item fs-6' href="/resource-hub/#media" onClick={closeOffcanvas}>Media</Link></li>
+                </ul>
+              </li>
               <li className="nav-item">
-              <Link className='me-4 nav-link fs-6' href="/join-us">Join Us</Link>
-              </li><hr/>
+                <Link className='me-4 nav-link fs-6' href="/join-us">Join Us</Link>
+              </li>
               <li className="nav-item">
-              <Link className='me-4 nav-link fs-6' href="/contact-us">Contact Us</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
+                <Link className='me-4 nav-link fs-6' href="/contact-us">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
-  </div>
-</div>
 
-
-
+      <div className={`offcanvas offcanvas-start ${offcanvasOpen ? 'show' : ''}`} data-bs-scroll="true" data-bs-backdrop="false" tabIndex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+        <div className="offcanvas-header">
+          <Image src='./images/cac_Logo.svg' width={200} height={50} alt='cac log' className='img_logo' />
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={toggleOffcanvas}></button>
+        </div>
+        <div className="offcanvas-body">
+          <nav className="">
+            <div className="container-fluid">
+              <div className="navbar">
+                <ul className="navbar-nav" style={{ width: '100%' }}>
+                  <li className="nav-item"><Link href="/" className="me-4 nav-link fs-6">Home</Link></li>
+                  <hr />
+                  <li className="nav-item">
+                    <Link href="/about-us" className="me-4 nav-link fs-6">About Us</Link>
+                  </li>
+                  <hr />
+                  <li className="nav-item">
+                    <Link href="/action" className="me-4 nav-link fs-6">Our Action</Link>
+                  </li>
+                  <hr />
+                  <li className="nav-item">
+                    <Link href="/impact" className="me-4 nav-link fs-6">Our Impact</Link>
+                  </li>
+                  <hr />
+                  <li className="nav-item dropdown mx-1">
+                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Our Initiatives
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li><Link className='dropdown-item fs-6' href="/shilp">SHILP</Link></li>
+                      <li><hr className="dropdown-divider" /></li>
+                      <li><Link className='dropdown-item fs-6' href="/economic-resilience">Economic resilience</Link></li>
+                      <li><hr className="dropdown-divider" /></li>
+                      <li><Link className='dropdown-item fs-6' href="https://covidactioncollab.org/vaxnow/" target='_blank'>VaxNow</Link></li>
+                      <li><hr className="dropdown-divider" /></li>
+                      <li><Link className='dropdown-item fs-6' href="https://www.precisionhealth.in/" target='_blank'>Precision Health</Link></li>
+                    </ul>
+                  </li>
+                  <hr />
+                  <li className="nav-item dropdown mx-1">
+                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Resource Hub
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li><Link className='dropdown-item fs-6' href="/resource-hub/#base">Knowledgebase</Link></li>
+                      <li><hr className="dropdown-divider" /></li>
+                      <li><Link className='dropdown-item fs-6' href="/resource-hub/#media" onClick={closeOffcanvas}>Media</Link></li>
+                    </ul>
+                  </li>
+                  <hr />
+                  <li className="nav-item">
+                    <Link className='me-4 nav-link fs-6' href="/join-us">Join Us</Link>
+                  </li>
+                  <hr />
+                  <li className="nav-item">
+                    <Link className='me-4 nav-link fs-6' href="/contact-us">Contact Us</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/app/impact/Ourimpact/Banner.js
+++ src/app/impact/Ourimpact/Banner.js
@@ -1,92 +1,100 @@
 // components/Banner.js
-"use client"
-import React from 'react';
-import { Col,Row, Container } from 'react-bootstrap';
-import CountUp from "react-countup"
-import { Navbar, NavDropdown } from 'react-bootstrap';
+"use client";
+import React from "react";
+import { Col, Row, Container } from "react-bootstrap";
+import CountUp from "react-countup";
+import { Navbar, NavDropdown } from "react-bootstrap";
 
 function Banner() {
   return (
-    <Container className='banner-bg d-flex align-items-end g-0' fluid style={{
-      backgroundImage: 'url(/images/imapact-banner.png)'
-    }}>
-      <Container style={{
-  backgroundImage: 'linear-gradient(90deg, #1c7c9cf0 28%, #fd9c00e0 91%)'
-}} className='g-0 middle pt-5' fluid>
-<Row >
-          <Col lg={2} className='border-end'>
-        <CountUp
-                  start={0}
-                  end={375}
-                  separator=" "
-                  prefix=""
-                  suffix=""
-                  className="increment-impact"
+    <Container
+      className="banner-bg d-flex align-items-end"
+      fluid
+      style={{backgroundImage: "url(/images/imapact-banner.png)"}}
+    >
+      <Container fluid
+        style={{
+          backgroundImage: "linear-gradient(90deg, #1c7c9cf0 28%, #fd9c00e0 91%)",
+        }}
+        
+        className="p-0 middle pt-5"
+        
+      >
+        <Row className="">
+          <Col lg={2} className="border-end">
+            <CountUp
+              start={0}
+              end={375}
+              separator=" "
+              prefix=""
+              suffix=""
+              className="increment-impact"
             ></CountUp>
-            <p className='fs-4 fw-light'>Partners of CAC</p>
-        </Col>
-        <Col lg={2} className='border-end'>
-        <CountUp
-                  start={0}
-                  end={34}
-                  separator=" "
-                  prefix=""
-                  suffix=""
-                  className="increment-impact"
+            <p className="fs-4 fw-light">Partners of CAC</p>
+          </Col>
+          <Col lg={2} className="border-end">
+            <CountUp
+              start={0}
+              end={34}
+              separator=" "
+              prefix=""
+              suffix=""
+              className="increment-impact"
             ></CountUp>
-            <p className='fs-4 fw-light'>States & UTs</p>
-        </Col>
-        <Col lg={2} className='border-end'>
-        <CountUp
-                  start={0}
-                  end={13}
-                  separator=" "
-                  prefix=""
-                  suffix=""
-                  className="increment-impact"
+            <p className="fs-4 fw-light">States & UTs</p>
+          </Col>
+          <Col lg={2} className="border-end">
+            <CountUp
+              start={0}
+              end={13}
+              separator=" "
+              prefix=""
+              suffix=""
+              className="increment-impact"
             ></CountUp>
-            <p className='fs-4 fw-light'>Vulnerable Communities</p>
-        </Col>
-        <Col lg={2} className='border-end'>
-        <CountUp
-                  start={0}
-                  end={2000}
-                  separator=","
-                  prefix=""
-                  suffix="+"
-                  className="increment-impact"
+            <p className="fs-4 fw-light">Vulnerable Communities</p>
+          </Col>
+          <Col lg={2} className="border-end">
+            <CountUp
+              start={0}
+              end={2000}
+              separator=","
+              prefix=""
+              suffix="+"
+              className="increment-impact"
             ></CountUp>
-            <p className='fs-4 fw-light'>Volunteers</p>
-        </Col>
-        <Col lg={2}>
-        <CountUp
-                  start={0}
-                  end={28}
-                  separator=" "
-                  prefix=""
-                  suffix="M"
-                  className="increment-impact"
+            <p className="fs-4 fw-light">Volunteers</p>
+          </Col>
+          <Col lg={2}>
+            <CountUp
+              start={0}
+              end={28}
+              separator=" "
+              prefix=""
+              suffix="M"
+              className="increment-impact"
             ></CountUp>
-            <p className='fs-4 fw-light'>Services Instances</p>
-        </Col>
-        <Col lg={2} className='border-start'>
-        <CountUp
-                  start={0}
-                  end={46}
-                  separator=","
-                  prefix="$"
-                  suffix="M"
-                  className="increment-impact"
+            <p className="fs-4 fw-light">Services Instances</p>
+          </Col>
+          <Col lg={2} className="border-start">
+            <CountUp
+              start={0}
+              end={46}
+              separator=","
+              prefix="$"
+              suffix="M"
+              className="increment-impact"
             ></CountUp>
-            <p className='fs-4 fw-light'>Resources<br/>Raised</p>
-        </Col>
-      </Row>
-
+            <p className="fs-4 fw-light">
+              Resources
+              <br />
+              Raised
+            </p>
+          </Col>
+        </Row>
       </Container>
-     
-
     </Container>
   );
-};
+}
 
 export default Banner;
