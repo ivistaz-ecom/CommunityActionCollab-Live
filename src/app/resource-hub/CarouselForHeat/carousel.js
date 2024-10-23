7efb83fd096a893c952c25ac467dc42d16ed8e29
Bhavankumarg <gbhavanbkr@gmail.com>
2024-10-23 15:09:17 +0530

    resource hub

---
+++ src/app/resource-hub/CarouselForHeat/carousel.js
@@ -0,0 +1,39 @@
+import { Carousel, Card } from "react-bootstrap";
+import Link from 'next/link';
+import React from 'react';
+import './MyCarousel.css'; // Add this CSS file for custom styles
+import { heatflyer } from "../../../../utils/data"; // Assuming heatflyer is an array of objects
+
+const MyCarousel = () => {
+  return (
+    <>
+      <Carousel 
+        indicators={false} 
+        nextIcon={<span aria-hidden="true" className="carousel-control-next-icon custom-arrow" />} 
+        prevIcon={<span aria-hidden="true" className="carousel-control-prev-icon custom-arrow" />}
+      >
+        {heatflyer.map((item, index) => (
+          <Carousel.Item key={index}>
+            <Card className="border-0 text-center mt-4">
+              <Card.Img
+                variant="top"
+                className="img-know"
+                src={item.thumbnail}
+              />
+              <Card.Body>
+                <Card.Title className="fs-5 fw-light mb-4 main-color">
+                  {item.title}
+                </Card.Title>
+                <Link href={item.pdflink} passHref  className="btn-know" target="_blank">
+                 Know More
+                </Link>
+              </Card.Body>
+            </Card>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </>
+  );
+}
+
+export default MyCarousel;
