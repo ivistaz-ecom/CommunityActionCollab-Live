5c55cdb51562e506c3476b81e1a2c8246945e360
Bhavankumarg <gbhavanbkr@gmail.com>
2025-01-20 11:50:38 +0530

    CS Reddy place changed to bottom

--- src/app/about-us/abouComponent/Ourvision.js
+++ src/app/about-us/abouComponent/Ourvision.js
@@ -6,7 +6,7 @@ import { Tab, Tabs } from 'react-bootstrap';
 
 function Ourvision() {
   const teamMembers = [
-    { name: 'CS Reddy', role: 'Founder and CEO, Mahila Abhivruddhi Society, Andhra Pradesh (APMAS)  and President, Sadhikaratha Foundation', image: '/images/cs-reddy.jpg' },
+    
     { name: 'Arvind Kumar Pandian', role: 'Project Manager, Strategic Partnerships, USAID', image: './images/arvind_kumar.jpg' },
     { name: 'Deepali Khanna', role: 'Managing Director, Asia Regional Office, The Rockefeller Foundation', image: '/images/deepali.png' },
     { name: 'Madan Padaki', role: 'Entrepreneur, Co-Founder; 1Bridge, Global Alliance for Mass Entrepreneurship (GAME)', image: '/images/madan-1.png' },
@@ -21,6 +21,7 @@ function Ourvision() {
     { name: 'Hari Krishna N', role: 'Senior Advisor - Resilience, Climate Change, Anticipatory Action and HDP Nexus, World Food Programme', image: '/images/hari.jpg' },
     { name: 'Gayathri Vasudevan', role: 'Chairperson, LabourNet Services', image: '/images/gayatri.jpg' },
     { name: 'Dr Neeta Rao', role: 'Senior Health Lead, USAID, India', image: '/images/Dr-Neeta-Rao.jpeg' },
+    { name: 'CS Reddy', role: 'Founder and CEO, Mahila Abhivruddhi Society, Andhra Pradesh (APMAS)  and President, Sadhikaratha Foundation', image: '/images/cs-reddy.jpg' },
     // Add more team members as needed
   ];
   return (
